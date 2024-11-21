b1fff26e1965e3edac3f37b54c2283f7916e602e
JosueTercero95i <josue.tercero95i@std.uni.edu.ni>
2024-11-20 21:51:22 -0600

    first

---
+++ ConsoleApp9/winHilos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace TransferenciaBancariaSimulacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Banco JNTA...");
+
+            Thread transferencia1 = new Thread(() => RealizarTransferencia("Transferencia a cuenta A", 3000));
+            Thread mensaje = new Thread(() => RealizarTransferencia("Validando credenciales", 5000)); 
+            Thread mensaje2 = new Thread(() => RealizarTransferencia("Estableciendo una conección segura", 7000)); 
+
+            transferencia1.Start();
+            mensaje.Start();
+            mensaje2.Start();
+
+            transferencia1.Join();
+            mensaje.Join();
+            mensaje2.Join();
+
+            Console.WriteLine("¡su transacción se ha completado exitosamente!");
+        }
+
+        static void RealizarTransferencia(string nombreTransferencia, int tiempo)
+        {
+            Console.WriteLine($"{nombreTransferencia} está siendo procesado...");
+            Thread.Sleep(tiempo); 
+            Console.WriteLine($"-------------------------");
+            Console.WriteLine($"{nombreTransferencia} completado.");
+            Console.WriteLine($"-------------------------");
+        }
+    }
+}
---
+++ ConsoleApp9/winTask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class winTask
+    {
+        static async Task Main(string[] args)
+        {
+            await RealizarTarea("Tarea 1", 1000); 
+            await RealizarTarea("Tarea 2", 5000); 
+            await RealizarTarea("Tarea 3", 10000); 
+
+            Console.WriteLine("Todas las tareas han finalizado.");
+        }
+
+        static async Task RealizarTarea(string nombre, int tiempo)
+        {
+            Console.WriteLine($"-------------------------");
+
+            Console.WriteLine($"{nombre} comenzada.");
+
+            Console.WriteLine($"-------------------------");
+
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"{nombre}: Escribiendo {i}/5...");
+                await Task.Delay(tiempo / 5); 
+            }
+
+            Console.WriteLine($"{nombre} completada.");
+        }
+    }
+}
